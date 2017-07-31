1997f835860c124b656b80f2490e20ed6e156a8a
Ronald Nicholson <rhn@nicholson.com>
2017-07-31 10:55:17 -0700

    Initial commit

---
+++ auv3test5/MyV3AudioUnit5.h
@@ -0,0 +1,13 @@
+//
+//  MyV3AudioUnit5.h
+//  auv3test5
+//
+//  Created by Ronald Nicholson on 7/31/17.
+//  Copyright © 2017 HotPaw Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyV3AudioUnit5 : NSObject
+
+@end
---
+++ auv3test5/MyV3AudioUnit5.m
@@ -0,0 +1,13 @@
+//
+//  MyV3AudioUnit5.m
+//  auv3test5
+//
+//  Created by Ronald Nicholson on 7/31/17.
+//  Copyright © 2017 HotPaw Productions. All rights reserved.
+//
+
+#import "MyV3AudioUnit5.h"
+
+@implementation MyV3AudioUnit5
+
+@end
