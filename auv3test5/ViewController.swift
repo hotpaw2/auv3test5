7564794ec914a5ad75fd82a2ae7518496cbd648b
Ronald Nicholson <rhn@nicholson.com>
2017-07-31 09:35:37 -0700

    Initial Commit

---
+++ auv3test5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  auv3test5
+//
+//  Created by Ronald Nicholson on 7/31/17.
+//  Copyright © 2017 HotPaw Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
